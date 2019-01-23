135785d01374a47c6f04c942e3970a543a96dfaa
uglyding2 <uglyding2@naver.com>
2019-01-23 12:08:00 +0900

    로그인 기능 구현 - 유저 정보 가져오기

--- spring-test/src/main/java/kr/green/springtest/MainController.java
+++ spring-test/src/main/java/kr/green/springtest/MainController.java
@@ -1,18 +1,18 @@
 package kr.green.springtest;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.green.springtest.service.AccountService;
+import kr.green.springtest.vo.AccountVo;
+
 @Controller
 public class MainController {
+	@Autowired
+	AccountService accountService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String homeGet(Model model) {
@@ -20,8 +20,9 @@ public class MainController {
 	}
 	
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public String homePost(Model model, String id, String pw) {
-		System.out.println("id : " + id +" pw : " + pw);
+	public String homePost(Model model, AccountVo loginInfo) {
+		AccountVo user = accountService.signin(loginInfo);
+		model.addAttribute("user", user);
 		return "redirect:/";
 	}
 }
--- spring-test/src/main/java/kr/green/springtest/service/AccountServiceImp.java
+++ spring-test/src/main/java/kr/green/springtest/service/AccountServiceImp.java
@@ -1,8 +1,25 @@
 package kr.green.springtest.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import kr.green.springtest.dao.AccountDao;
+import kr.green.springtest.vo.AccountVo;
 
 @Service
 public class AccountServiceImp implements AccountService{
+	@Autowired
+	AccountDao accountDao;
+	@Autowired
+	BCryptPasswordEncoder passwordEncoder;
+	
+	@Override
+	public AccountVo signin(AccountVo loginInfo) {
+		AccountVo user = accountDao.getAccount(loginInfo.getId()); // 비밀번호가 암호화 되어있기 때문에 id로 확인.
+		if(passwordEncoder.matches(loginInfo.getPw(), user.getPw()))
+			return user;
+		return null;
+	}
 	
 }
